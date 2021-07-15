bf946ddd5900e5589dfce10951c2ce1e911a7c9e
williamferpc <willi_9212@hotmail.com>
2021-07-14 21:36:52 -0500

    operadores aritmeticos_14_07_2021

---
+++ main.js
@@ -0,0 +1,52 @@
+//Pedir 2 datos por pantalla y almacenarlo en dos variables 
+//diferentes:
+let num1 = parseInt(prompt("Ingrese numero 1"));
+let num2 = parseInt(prompt("Ingrese numero 2"));
+alert(`Oprime F12 para ver la consola`);
+
+//Realizar todas las Operacion aritmeticas vistas en la clase
+
+//Variable 1(num1)  Variable 2 (num2)
+console.log(`numero 1 => ${num1}, numero 2 => ${num2}`);
+
+//Adición
+console.log(`Adicion  ${num1}+${num2} => ${(num1)+(num2)}`);
+
+//Division
+console.log(`Division ${num1} / ${num2} => ${(num1)/(num2)}`);
+
+//Exponenciacion
+console.log(`Exponenciacion ${num1} ** ${num2} => ${(num1)**(num2)}`);
+
+//Multiplicacion
+console.log(`Multiplicacion ${num1} * ${num2} => ${(num1)*(num2)}`);
+
+//Residuo
+console.log(`Residuo ${num1} % ${num2} => ${(num1)%(num2)}`);
+
+//Sustraccion
+console.log(`Sustraccion ${num1} - ${num2} => ${(num1)-(num2)}`);
+
+//Negación
+console.log(`Negación => ${-(num1)}`);
+
+//Positivo
+console.log(`Absoluto => ${+(num1)}`);
+
+//Incremento
+console.log(`Post-Incremento ${num1}++=> ${(num1++)}`);
+
+//Post-Decremento
+console.log(`Post-Decremento ${num1}-- => ${num1--}`);
+
+//pre-Incremento
+console.log(`Pre-Incremento ++${num2}=> ${++num2}`);
+
+//pre-Decremento
+console.log(`pre-Decremento --${num2} => ${--num2}`);
+//Mostra en la consola todos los posibles resultados
+
+//Extra: Por medio de una venta emergente(alert) Dar la maxima
+//informacion del desarrallodar del aplicativo en cuestion
+alert(`Desarrollador por:\nWilliam Fernando Peña Corzo © Todos los derechos reservados`);
+
